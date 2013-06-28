26218a9b8f2ec3e193f2be61eb7cef5554414dcf
Toby Inkster <mail@tobyinkster.co.uk>
2013-06-28 15:46:32 +0100

    bump version; changelog

--- lib/MooseX/DeclareX/Plugin/abstract.pm
+++ lib/MooseX/DeclareX/Plugin/abstract.pm
@@ -2,7 +2,7 @@ package MooseX::DeclareX::Plugin::abstract;
 
 BEGIN {
 	$MooseX::DeclareX::Plugin::abstract::AUTHORITY = 'cpan:TOBYINK';
-	$MooseX::DeclareX::Plugin::abstract::VERSION   = '0.003';
+	$MooseX::DeclareX::Plugin::abstract::VERSION   = '0.004';
 }
 
 use Moose;
